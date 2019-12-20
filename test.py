86ee92568de2d12a69c1a4ab9c14a12da5588c23
alexc21 <39373214+alexc21@users.noreply.github.com>
2019-12-20 16:27:08 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+random.seed(a=int(), version=2)
+somme_joueur= 2
+nmb_joueur= random.randrange(50)
+nombre = random.randrange(50)
+
+print(nmb_joueur)
+print(nombre)
+
+if nmb-joueur < 50:
+    if nombre % 2 and nombre = nmb_joueur:
+        print("vous avez gagner")
+    elif nombre != nmb_joueur and:
+        print("vous avez perdu")
